15aa683cb5d7e724e72e239027bcb455277334c7
Dylan Bernard <dtbernard11@gmail.com>
2018-08-15 09:53:17 -0700

    add snippet for test cases

--- UltiSnips/python.snippets.py
+++ UltiSnips/python.snippets.py
@@ -10,3 +10,11 @@ from sniputil import bsnip
 bsnip('patch', 'MagicMock @patch', r'''
 @patch('${1:object}')
 ''')
+
+###########
+# Testing #
+###########
+bsnip('test', 'def test_...(self)', r'''
+def test_${1:funcname}(self):
+    pass
+''', aliases=['t', 'tc'])
