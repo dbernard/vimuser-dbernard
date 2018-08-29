74bb538b4faba03a924abaab2db4a0153d339314
Dylan Bernard <dtbernard11@gmail.com>
2018-08-28 20:48:54 -0700

    add some Django testing snips

--- UltiSnips/python.snippets.py
+++ UltiSnips/python.snippets.py
@@ -3,6 +3,21 @@
 
 from sniputil import bsnip
 
+##########
+# Django #
+##########
+bsnip('setUpTestData', 'Django testing setUpTestData', r'''
+@classmethod
+def setUpTestData(cls)
+    super(${1:classname}, cls).setUpTestData()
+    ${2:setup}
+''', aliases=['setuptestdata'])
+
+bsnip('setUp', 'Django testing setUp', r'''
+def setUp(self):
+    super(${1:class}, self).setUp()
+    ${2:setup}
+''')
 
 #############
 # MagicMock #
